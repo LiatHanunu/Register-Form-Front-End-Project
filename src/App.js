eefc549448206098709d6f52b2a92576093d0a58
LiatHanunu <liat.hanunu01@gmail.com>
2022-10-26 13:45:59 +0300

    adding new User Deatils form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import NewUserDetails from './Components/NewUserDetails';
 import RegisterForm from './Components/RegisterForm';
 
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <NewUserDetails></NewUserDetails>
     </>
 
   );
--- src/Components/NewUserDetails.js
+++ src/Components/NewUserDetails.js
@@ -1,4 +1,56 @@
 
-export default function NewUserDetails(){
-    
+
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Form from 'react-bootstrap/Form';
+import Row from "react-bootstrap/esm/Row";
+
+export default function NewUserDetails(props) {
+    return (<Container className="d-flex justify-content-center p-5">
+
+        <Form className='rounded border p-3 m-2 bg-light'>
+            <h3>New Profile Details</h3>
+            <hr></hr>
+            <Row>
+                <Col lg={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control disabled name="email" defaultValue={props.firstName} />
+                    </Form.Group>
+                </Col>
+                <Col lg={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control disabled name="email" defaultValue={props.lastName} />
+                    </Form.Group>
+                </Col>
+                <Col lg={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Gender</Form.Label>
+                        <Form.Control disabled name="email" defaultValue={props.gender} />
+                    </Form.Group>
+                </Col>
+                <Col lg={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Birthdate</Form.Label>
+                        <Form.Control disabled name="email" defaultValue={props.birthdate} />
+                    </Form.Group>
+                </Col>
+                <Col lg={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control disabled name="email" defaultValue={props.email} />
+                    </Form.Group>
+                </Col>
+            </Row>
+            <hr></hr>
+            <Form.Group className='d-flex flex-row-reverse'>
+                <Button variant="primary" type="submit">
+                    Create another new user
+                </Button>
+            </Form.Group>
+        </Form>
+    </Container>)
+
 }
